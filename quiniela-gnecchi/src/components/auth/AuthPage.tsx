d601573fa95fd2ccec553a269964766f04d3010f
quinielasgnecchi <quinielasgnecchi@gmail.com>
2026-06-12 15:23:40 -0600

    Update AuthPage.tsx

--- quiniela-gnecchi/src/components/auth/AuthPage.tsx
+++ quiniela-gnecchi/src/components/auth/AuthPage.tsx
@@ -47,8 +47,8 @@ export default function AuthPage() {
         style={{ backgroundImage: `url(${wallpaper})` }}
       />
       
-      {/* Contenedor principal: Altura reducida un 20% (py-[74px]) y ancho ajustado a max-w-[224px] */}
-      <div className="w-full max-w-[224px] px-4 py-[74px] rounded-[36px] bg-black border border-black relative z-10">
+      {/* Contenedor principal: Altura reducida un 10% adicional (py-[66px]) manteniendo el ancho fijo en max-w-[224px] */}
+      <div className="w-full max-w-[224px] px-4 py-[66px] rounded-[36px] bg-black border border-black relative z-10">
         
         {/* Cabecera: Desplazada hacia arriba (-mt-2) y con margen inferior reducido (mb-2) */}
         <div className="text-center -mt-2 mb-2">
